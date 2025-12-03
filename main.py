140d433d82a69608e01d84c364adc61167ad14d2
Daniel Dawson Jensen <dadj@aarhus.dk>
2025-12-03 07:37:50 +0100

    updated ssl

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ This is the main entry point for the process
 import asyncio
 import logging
 import sys
-import os
 
 from automation_server_client import AutomationServer, Workqueue
 from mbu_rpa_core.exceptions import BusinessError, ProcessError
@@ -19,8 +18,6 @@ from processes.process_item import process_item
 from processes.queue_handler import concurrent_add, retrieve_items_for_queue
 
 logger = logging.getLogger(__name__)
-
-os.environ["REQUESTS_CA_BUNDLE"] = R"C:\certs\aarhus_root.pem"
 
 
 # ╔══════════════════════════════════════════════╗
